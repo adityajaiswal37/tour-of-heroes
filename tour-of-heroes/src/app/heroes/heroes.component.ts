31677cf17f0473e84b802cfe11176fa66e64a4a4
Aditya <adijaiswal0307@gmail.com>
2023-12-07 13:06:49 +0530

    4th

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
+import { HEROES } from './../mock-heroes';
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit{
+  heroes:Hero[] = [
+    {name:'Iron Man',id:1},
+    {name:'Hulk',id:2},
+    {name:'Thor',id:3},
+    {name:'Black Widow',id:4},
+    {name:'Hawkeye',id:5},
+    {name:'Captain',id:6},
+    {name:'Spiderman',id:7}
+]
   ngOnInit(): void {
     
   }
   constructor(){}
 
-  hero: Hero = {name:'Iron Man',id:1}
 
 }
---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+export const HEROES: Hero[]=[
+    {name:'Iron Man',id:1},
+    {name:'Hulk',id:2},
+    {name:'Thor',id:3},
+    {name:'Black Widow',id:4},
+    {name:'Hawkeye',id:5},
+    {name:'Captain',id:6},
+    {name:'Spiderman',id:7}
+]
